3280a676db6bff0b8bc8e446d18bb29bea4e3a41
mohammad-damra <mohammad.s.damra@gmail.com>
2021-01-01 21:37:57 +0200

    move from part 4

--- part_4/part_4.js
+++ part_4/part_4.js
@@ -11,11 +11,23 @@ return 'NO' if the parentheses in the input string are not balanced,.
 
 console.log('PART 4');
 
-const isABalancedString = () => {
-  // WRITE YOUR CODE BELOW THIS LINE
+const isABalancedString = (str) => {
+	// WRITE YOUR CODE BELOW THIS LINE
+	strArr = str.split('');
+	let arr1 = [];
+	let arr2 = [];
+	if (strArr.length % 2 !== 0) return 'NO';
+	for (let i = 0; i < strArr.length; i++) {
+		if (i < strArr.length / 2) arr1.push(strArr[i]);
+		else arr2.push(strArr[i]);
+	}
+	console.log('arr1', arr1);
+	console.log('arr2', arr2.reverse());
+	console.log('object', arr1[0] == arr2[0]);
 };
 
 /*
+console.log('arr2', arr2)
 
 Examples:
 
